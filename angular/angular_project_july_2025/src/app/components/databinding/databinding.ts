fb7f52940db81d565c700458c54cd96c91d05b32
TarunNagaSaiChadaram <tarunchadaram2001@gmail.com>
2025-08-16 01:26:21 -0400

    day03_aug15

---
+++ angular/angular_project_july_2025/src/app/components/databinding/databinding.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-databinding',
+  imports: [
+    FormsModule
+  ],
+  templateUrl: './databinding.html',
+  styleUrl: './databinding.css'
+})
+export class Databinding {
+  myName: string = 'Virat Kohli';
+  img_url: string = 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/1024px-Angular_full_color_logo.svg.png?20160527092314';
+
+  flag: boolean = false;
+
+  isitvis:boolean=true;
+
+  textmax:string="";
+
+  textmaxlen:number=100;
+
+  stateselected:string="";
+
+  firstnumber:number=0;
+  secondnumber:number=0;
+  opt:string=""
+
+  totoggel(){
+    this.isitvis=!this.isitvis;
+  }
+
+  addToCart() {
+    alert('Your Item is added to Cart!!!');
+  }
+}
